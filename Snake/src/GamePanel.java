40cbf4778104c4021e731848ead516df7c87dcca
Vedantlearn <112773665+Vedantlearn@users.noreply.github.com>
2024-01-19 23:50:28 +0530

    Update GamePanel.java

--- Snake/src/GamePanel.java
+++ Snake/src/GamePanel.java
@@ -2,7 +2,6 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 import java.util.Random;
-
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
