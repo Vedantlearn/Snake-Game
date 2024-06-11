493ed0ddb3b767c11bcb2409e4f83d33fdadcc58
Vedantlearn <112773665+Vedantlearn@users.noreply.github.com>
2024-06-11 23:55:42 +0530

    Update GameFrame.java

--- Snake/src/GameFrame.java
+++ Snake/src/GameFrame.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 public class GameFrame extends JFrame {
 
 	GameFrame() {
-
+ 
 		this.add(new GamePanel());
 		this.setTitle("SNAKE");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
